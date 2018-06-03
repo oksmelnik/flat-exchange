29d83ef54bfe34faa9d5750d9c81fe20acfacd11
oksmelnik <melnik.oksana@gmail.com>
2018-06-03 13:31:24 +0200

    fb login

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
-import 'react-bootstrap'
+import Facebook from './components/Facebook'
+
 
 
 class App extends Component {
+
+
   render() {
+
+
     return (
       <div className="App">
       <Header className='Header'/>
-      </div>
+      <Facebook />
+
+  </div>
     );
   }
 }
