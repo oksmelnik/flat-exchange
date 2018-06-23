42b0a66270f2194cf0ccacbe3c730ab3113b1966
oksmelnik <melnik.oksana@gmail.com>
2018-06-23 22:02:56 +0200

    flat sidebar

--- client/src/components/FlatPage.js
+++ client/src/components/FlatPage.js
@@ -3,6 +3,7 @@ import { Image, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import {connect} from 'react-redux'
 import './Profile.css'
 import {Redirect, Link} from 'react-router-dom'
+import Sidebar from './Sidebar'
 
 class Profile extends Component {
   render() {
@@ -14,54 +15,59 @@ class Profile extends Component {
 
 
     return (
-      <div className='panel panel-default'>
-        <div className="panel-heading">
-          Create your flat
-          </div>
-        <div className='panel-body'>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-md-4 select'>
-                <div className="form-group">
-                  <label>Home Type</label>
-                    <select className="form-control">
-                      <option value="Appartment">Appartment</option>
-                      <option value="House">House</option>
-                      <option value="Bed&Breakfast">Bed&Breakfast</option>
-                      </select>
-                    </div>
-                  </div>
+      <div className='container'>
+      <div className='row'>
+      <div className='col-md-2'>
+        <Sidebar/>
+        </div>
+      <div className='col-md-10'>
+        <div className='panel panel-default'>
+          <div className="panel-heading">
+            Create your flat
+            </div>
+          <div className='panel-body'>
+            <div className='container'>
+              <div className='row'>
                 <div className='col-md-4 select'>
                   <div className="form-group">
-                    <label>Room Type</label>
+                    <label>Home Type</label>
                       <select className="form-control">
-                        <option value="Entire">Entire</option>
-                        <option value="Shared">Shared</option>
+                        <option value="Appartment">Appartment</option>
+                        <option value="House">House</option>
+                        <option value="Bed&Breakfast">Bed&Breakfast</option>
                         </select>
                       </div>
                     </div>
                   <div className='col-md-4 select'>
                     <div className="form-group">
-                      <label>Accomodate</label>
+                      <label>Room Type</label>
                         <select className="form-control">
-                          <option value="1">1</option>
-                          <option value="2">2</option>
-                          <option value="3">3</option>
-                          <option value="4">4</option>
-                          <option value="5">5</option>
+                          <option value="Entire">Entire</option>
+                          <option value="Shared">Shared</option>
                           </select>
                         </div>
                       </div>
-                  <button type="submit" className="btn btn-normal">Submit</button>
-
-
-
+                  <div className='col-md-4 select'>
+                    <div className="form-group">
+                      <label>Accomodate</label>
+                          <select className="form-control">
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            </select>
+                          </div>
+                        </div>
+                    <button type="submit" className="btn btn-normal">Submit</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-      </div>
-
-      )}}
+    )}}
 
 const mapStateToProps = state => ({
     user: state.user === null ? null : state.user.user
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -15,29 +15,27 @@ class Profile extends Component {
 
     return (
       <div className="container"
-      style={{  }}>
-      <div className="row">
-        <div className="col-md-3">
-          <Panel bsStyle="primary">
-            <Panel.Heading>Verification</Panel.Heading>
-            <Panel.Body>
-              <ListGroup>
-                  <ListGroupItem>Email Address {user.email}</ListGroupItem>
-                  <ListGroupItem>Phone Number</ListGroupItem>
-                  </ListGroup>
-            </Panel.Body>
-          </Panel>
-        </div>
-    <div className="col-md-6">
+        style={{  }}>
+        <div className="row">
+          <div className="col-md-3">
+            <Panel bsStyle="primary">
+              <Panel.Heading>Verification</Panel.Heading>
+              <Panel.Body>
+                <ListGroup>
+                    <ListGroupItem>Email Address {user.email}</ListGroupItem>
+                    <ListGroupItem>Phone Number</ListGroupItem>
+                    </ListGroup>
+              </Panel.Body>
+            </Panel>
+            </div>
+          <div className="col-md-1">
             <Image className="avatar-small" src={user.picture} rounded/>
-
-            <div className="col-ms-3">
+              </div>
+          <div className="col-ms-3">
             <h2>{user.name}</h2>
             </div>
-
-</div>
-</div>
-</div>
+          </div>
+          </div>
       )}}
 
 const mapStateToProps = state => ({
