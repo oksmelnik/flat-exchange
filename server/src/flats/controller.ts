d40c9c5c4e8eb8c92adc4e17a85ea263b1968a34
oksmelnik <melnik.oksana@gmail.com>
2018-07-13 13:36:30 +0200

    default is_active

--- server/src/flats/controller.ts
+++ server/src/flats/controller.ts
@@ -1,33 +1,48 @@
 import {
-JsonController, Authorized, CurrentUser, Post, Param, BadRequestError, HttpCode, NotFoundError, ForbiddenError, Get,
-Body, Put
-} from 'routing-controllers'
-import { Flat} from './entity'
-import { User } from '../users/entity'
+  JsonController,
+  Authorized,
+  CurrentUser,
+  Post,
+  Param,
+  BadRequestError,
+  HttpCode,
+  NotFoundError,
+  ForbiddenError,
+  Get,
+  Body,
+  Put
+} from "routing-controllers";
+import { Flat } from "./entity";
+import { User } from "../users/entity";
 
-export const baseUrl = 'http://localhost:4008'
-
+export const baseUrl = "http://localhost:4008";
 
 @JsonController()
 export default class FlatController {
+  @Get("/flats/:id([0-9]+)")
+  @HttpCode(200)
+  getFlat(@Param("id") id: number) {
+    return Flat.findOneById(id);
+  }
 
-  @Get('/flats/:id([0-9]+)')
-    @HttpCode(200)
-    getFlat(
-      @Param('id') id: number
-      ){
-        return Flat.findOneById(id)
-        }
+  @Get("/:id([0-9]+)/flats/")
+  @HttpCode(200)
+  async getAllFlat(@Param("id") userId: number) {
+    console.log(userId);
+    const user = await User.findOneById(userId);
+    if (!user) throw new BadRequestError("no user");
+    const flats = await Flat.find({
+      where: { user: user }
+    });
+    return flats;
+  }
 
-   @Post("/:id([0-9]+)/flats")
-   @HttpCode(201)
-   async addFlat(
-    @Param('id') userId: number,
-    @Body() flat: Partial <Flat>
-    ){
-    const user = await User.findOneById(userId)
+  @Post("/:id([0-9]+)/flats")
+  @HttpCode(201)
+  async addFlat(@Param("id") userId: number, @Body() flat: Partial<Flat>) {
+    const user = await User.findOneById(userId);
 
-    if(!user) throw new BadRequestError("User doesn't exist.")
+    if (!user) throw new BadRequestError("User doesn't exist.");
     const entity = await Flat.create({
       room_type: flat.room_type,
       home_type: flat.home_type,
@@ -35,19 +50,16 @@ export default class FlatController {
       address: flat.address,
       is_active: flat.is_active,
       user: user
-    }).save()
-      return entity
-    }
+    }).save();
+    return entity;
+  }
 
-    @Put('/flats/:id([0-9]+)')
-    @HttpCode(201)
-    async changeFlat(
-      @Param('id') id: number,
-      @Body() updates: Partial<Flat>
-    ) {
-       const flat = await Flat.findOneById(id)
-       if(!flat) throw new BadRequestError("Flat doesn't exist.")
-       const changedFlat = await Flat.merge(flat, updates).save()
-       return changedFlat
- }
+  @Put("/flats/:id([0-9]+)")
+  @HttpCode(201)
+  async changeFlat(@Param("id") id: number, @Body() updates: Partial<Flat>) {
+    const flat = await Flat.findOneById(id);
+    if (!flat) throw new BadRequestError("Flat doesn't exist.");
+    const changedFlat = await Flat.merge(flat, updates).save();
+    return changedFlat;
+  }
 }
--- server/src/flats/entity.ts
+++ server/src/flats/entity.ts
@@ -1,43 +1,47 @@
-import { BaseEntity, PrimaryColumn, PrimaryGeneratedColumn, Column, Entity, OneToMany, ManyToOne } from 'typeorm'
-import { IsString, IsBoolean, IsInt } from 'class-validator'
-import { User } from '../users/entity'
+import {
+  BaseEntity,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  Column,
+  Entity,
+  OneToMany,
+  ManyToOne
+} from "typeorm";
+import { IsString, IsBoolean, IsInt } from "class-validator";
+import { User } from "../users/entity";
 
 @Entity()
 export class Flat extends BaseEntity {
-
-  @PrimaryGeneratedColumn()
-  id?: number
+  @PrimaryGeneratedColumn() id?: number;
 
   @IsString()
-  @Column('text', { nullable: true })
-  home_type: string
+  @Column("text", { nullable: true })
+  home_type: string;
 
   @IsString()
-  @Column('text', { nullable: true })
-  room_type: string
+  @Column("text", { nullable: true })
+  room_type: string;
 
   @IsInt()
   @Column()
-  accomodate: number
+  accomodate: number;
 
   @IsString()
-  @Column('text', { nullable: true })
-  name: string
+  @Column("text", { nullable: true })
+  name: string;
 
   @IsString()
-  @Column('text', { nullable: true })
-  summary: string
+  @Column("text", { nullable: true })
+  summary: string;
 
   @IsString()
-  @Column('text', { nullable: true })
-  address: string
+  @Column("text", { nullable: true })
+  address: string;
 
   @IsBoolean()
-  @Column({ nullable: true })
-  is_active: boolean
+  @Column({ nullable: true, default: false })
+  is_active: boolean;
 
-  @ManyToOne(_ => User, user => user.flat, {onDelete: "CASCADE"})
-  user: User
-
-
-  }
+  @ManyToOne(_ => User, user => user.flat, { onDelete: "CASCADE" })
+  user: User;
+}
